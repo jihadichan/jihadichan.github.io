2f2dc4474013c6c66530f7bf031e46404571a6fe
dude <go@suckadick.com>
2020-07-26 19:43:56 +0200

    quick commit

--- repeater/script.js
+++ repeater/script.js
@@ -2,19 +2,20 @@ var current = $('#current');
 var from = $('#from');
 var to = $('#to');
 var playButton = $('#playpause');
+var loopMarker = $('#loop');
 var playData = [];
-var playDataIndex = -1;
+var playDataIndex = 0;
 var japanese = $('#japanese');
 var english = $('#english');
 var playingAudio;
 var isSessionRunning = false;
+var isInLoop = false;
 
 // checked / unchecked is reversed because of CSS
 function startStop() {
     if (playButton.is(':checked')) {
         if (isSessionRunning) {
-            decrementPlayDataIndex();
-            nextSentence();
+            playSameSentence();
         } else {
             run();
         }
@@ -27,18 +28,6 @@ function stop() {
     if (playingAudio) {
         playingAudio.pause();
         playingAudio.currentTime = 0;
-    }
-}
-
-function decrementPlayDataIndex() {
-    if (playDataIndex > -1) {
-        playDataIndex--;
-    }
-}
-
-function incrementPlayDataIndex() {
-    if (playDataIndex >= getTo()) {
-        playDataIndex++;
     }
 }
 
@@ -58,7 +47,12 @@ function run() {
     // Set up
     var localCurrent = parseInt(from.val());
     if (getTo() - localCurrent > 500) {
-        to.val(localCurrent + 500);
+        var plus500 = localCurrent + 500;
+        if (plus500 >= sentences.length - 1) {
+            to.val(sentences.length - 1);
+        } else {
+            to.val(plus500);
+        }
     }
 
     // Create audio array to play
@@ -71,34 +65,67 @@ function run() {
             console.log("At index " + sentence.index + " - Failed to load " + sentence.fileName)
         }
     })
-    playDataIndex = -1;
+    // playDataIndex = 0;
     isSessionRunning = true;
-    nextSentence();
+    playSameSentence(); // i.e. playDataIndex == 0
 }
 
-function nextSentence() {
-    playDataIndex++;
+function nextSentence(addition) {
+    if (addition === undefined) {
+        throw "addition must be set";
+    }
+
+    addToPlayDataIndex(addition);
+    console.log("playDataIndex", playDataIndex)
+    console.log("playData", playData)
     if (playDataIndex === playData.length) {
-        playDataIndex = -1;
+        console.log("max!")
+        playDataIndex = 0;
         isSessionRunning = false;
         run();
         return;
     }
+
     var sentence = playData[playDataIndex];
+    console.log("index: ", sentence.index)
     current.text(sentence.index);
     japanese.text(sentence.japanese)
     english.text(sentence.english);
     playingAudio = sentence.audio;
-    sentence.audio.addEventListener('ended', nextSentence);
+    sentence.audio.addEventListener('ended', playNextSentence);
     sentence.audio.play();
+}
+
+function addToPlayDataIndex(addition) {
+    var result = playDataIndex + addition;
+    console.log(result)
+    if (result < 0 || result >= sentences.length - 1) {
+        return;
+    }
+    if (isInLoop) {
+        return;
+    }
+    playDataIndex += addition;
+}
+
+function playNextSentence() {
+    nextSentence(1);
+}
+
+function playPreviousSentence() {
+    nextSentence(-1);
+}
+
+function playSameSentence() {
+    nextSentence(0);
 }
 
 function setTotal() {
     $('#total').html(getMaxIndex());
 }
 
-function setCurrent() {
-    current.html(0);
+function setCurrent(value) {
+    current.html(value);
 }
 
 function setFromTo() {
@@ -143,43 +170,78 @@ function search(term) {
     console.log(JSON.stringify(resultSet, null, '\t'));
 }
 
+function toggleLoop() {
+    isInLoop = !isInLoop;
+    console.log("loop? ", isInLoop)
+}
+
+function disableLoop() {
+    isInLoop = false;
+}
+
+function enableLoop() {
+    isInLoop = true;
+}
+
+function togglePlayButton() {
+    if (playButton.is(':checked')) {
+        playButton.prop("checked", false);
+    } else {
+        playButton.prop("checked", true);
+    }
+}
+
 document.onkeyup = function (e) {
     if (e.code === "Space") {
-        if (playButton.is(':checked')) {
-            playButton.prop("checked", false);
-        } else {
-            playButton.prop("checked", true);
-        }
+        togglePlayButton();
         startStop();
     }
     if (e.code === "ArrowDown") {
         if (isSessionRunning) {
-            decrementPlayDataIndex();
             stop();
-            nextSentence();
+            playSameSentence();
         }
     }
     if (e.code === "ArrowLeft") {
         if (isSessionRunning) {
-            decrementPlayDataIndex();
-            decrementPlayDataIndex();
             stop();
-            nextSentence();
+            if (!playButton.is(':checked')) {
+                playButton.prop("checked", true);
+            }
+            if (isInLoop) {
+                disableLoop();
+                playPreviousSentence();
+                enableLoop();
+            } else {
+                playPreviousSentence();
+            }
         }
     }
     if (e.code === "ArrowRight") {
         if (isSessionRunning) {
-            incrementPlayDataIndex();
             stop();
-            nextSentence();
+            if (!playButton.is(':checked')) {
+                playButton.prop("checked", true);
+            }
+            if (isInLoop) {
+                disableLoop();
+                playNextSentence();
+                enableLoop();
+            } else {
+                playNextSentence();
+            }
         }
+    }
+    if (e.code === "ArrowUp") {
+        loopMarker.toggle();
+        toggleLoop();
     }
     if (e.code === "KeyE") {
         english.toggle();
     }
 }
 
-setCurrent();
+setCurrent(0);
 setTotal();
 setFromTo();
 
