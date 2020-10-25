f7172a430f3a17e83929c7aff9e135d83edb18f0
dude <go@suckadick.com>
2020-10-25 20:48:54 +0100

    quick commit

--- kanji/mnemonics.js
+++ kanji/mnemonics.js
@@ -1,6 +1,7 @@
 var searchField = $('#search-field');
 var resultSetElement = $('#result-set-container');
 var info = $('#info');
+var KATAKANA_HIRAGANA_SHIFT = "\u3041".charCodeAt(0) - "\u30a1".charCodeAt(0);
 
 function search() {
     var searchTerm = searchField.val().trim();
@@ -9,7 +10,7 @@ function search() {
         return;
     }
     var matches = collectMatches(searchTerm);
-    console.log(matches);
+    info.html("Results: " + matches.length);
     renderResults(matches);
 }
 
@@ -19,10 +20,10 @@ function renderResults(matches) {
     $(matches).each(function (index, entry) {
         html += "<tr>";
         html += "   <td style='text-align: center'>";
-        html += "       "+entry.k;
+        html += "       " + entry.k;
         html += "   </td>";
         html += "   <td class='info'>";
-        html += "       "+entry.m;
+        html += "       " + entry.m;
         html += "   </td>";
         html += "</tr>";
     });
@@ -32,8 +33,9 @@ function renderResults(matches) {
 }
 
 function collectMatches(searchTerm) {
-    console.log(searchTerm)
     var matches = [];
+    searchTerm = normalizeSearchTerm(searchTerm);
+    console.log(searchTerm)
     $(data).each(function (index, entry) {
         if (hasMatches(searchTerm, entry)) {
             matches.push(entry);
@@ -42,9 +44,41 @@ function collectMatches(searchTerm) {
     return matches;
 }
 
+function normalizeSearchTerm(searchTerm) {
+
+    var charArray = searchTerm.split("");
+    var resultArray = [];
+    $(charArray).each(function (index, ch) {
+        if (wanakana.isHiragana(ch)) {
+            resultArray.push(wanakana.toKatakana(ch));
+        } else if (wanakana.isKatakana(ch)) {
+            resultArray.push(wanakana.toHiragana(ch));
+        } else {
+            resultArray.push(ch);
+        }
+    });
+    var normalized = resultArray.join("");
+    if (normalized === searchTerm) {
+        return searchTerm;
+    }
+    return "(" + searchTerm + ")|(" + normalized + ")"
+}
+
 function hasMatches(searchTerm, entry) {
     entry = entry.k + ", " + entry.m;
     return entry.search(new RegExp(searchTerm, "i")) > -1;
+}
+
+function toHiragana(str) {
+    var result = "";
+    $.each(str.split(''), function (index, value) {
+        if (value > "\u30a0" && value < "\u30f7") {
+            result += String.fromCharCode(value.charCodeAt(0) + KATAKANA_HIRAGANA_SHIFT);
+        } else {
+            result += value;
+        }
+    });
+    return result;
 }
 
 searchField.keypress(function (e) {
@@ -53,5 +87,4 @@ searchField.keypress(function (e) {
     }
 });
 
-console.log("\\b" + "かい" + "\\b")
 
